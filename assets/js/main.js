272f295a5afa0244980092f0415e19a01adeb6f9
man0fcode <amirzm51@gmail.com>
2021-08-30 22:10:51 +0430

    styles and js qualification section base set

--- assets/js/main.js
+++ assets/js/main.js
@@ -46,5 +46,30 @@ skillsHeader.forEach((el) => {
     el.addEventListener('click', toggleSkills)
 })
 /*================= QUALIFICATION TABS =================*/
+const tabs = document.querySelectorAll('[data-target]'),
+    tabContents = document.querySelectorAll('[data-content]')
 
+tabs.forEach(tab => {
+    tab.addEventListener('click',()=>{
+        const target = document.querySelector(tab.dataset.target)
+        
+        tabContents.forEach(tabContents =>{
+            tabContents.classList.remove('qualification__active')
+        })
+        target.classList.add('qualification__active')
+
+        tab.forEach(tab =>{
+            tab.classList.remove('qualification__active')
+        })
+        tab.classList.add('qualification__active')
+    })
+})
 /*================= SERVICES MODAL =================*/
+
+/*================= PORTFOLIO SWIPER =================*/
+
+/*================= TESTIMONIAL =================*/
+
+/*================= SCROLL SECTION ACTIVE LINK =================*/
+
+/*================= CHANGE BACKGROUND HEADER =================*/
