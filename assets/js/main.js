81458d27a94df9aa3d60786e408d00e400780282
man0fcode <amirzm51@gmail.com>
2021-09-02 13:27:01 +0430

    scrollup button style and js set

--- assets/js/main.js
+++ assets/js/main.js
@@ -110,12 +110,40 @@ let swiperTestimonial = new Swiper(".testimonial__container", {
         clickable: true,
         dynamicBullets: true,
     },
-    breakpoints:{
-        568:{
+    breakpoints: {
+        568: {
             slidesPerView: 2,
         }
     }
 });
 /*================= SCROLL SECTION ACTIVE LINK =================*/
+const sections = document.querySelectorAll('section[id]')
 
+function scrollActive() {
+    const scrollY = window.pageYOffset
+
+    sections.forEach(current => {
+        const sectionHeight = current.offsetHeight
+        const sectionTop = current.offsetTop - 50;
+        sectionId = current.getAttribute('id')
+
+        if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
+            document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.add('active-link')
+        } else {
+            document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.remove('active-link')
+        }
+
+        window.addEventListener('scroll',scrollActive)
+    })
+}
 /*================= CHANGE BACKGROUND HEADER =================*/
+function scrollHeader(){
+    const nav = document.getElementById('header')
+    //if scroll >= 80 viewport -> scroll-header class will added to header tag
+    if(this.scrollY >= 80) nav.classList.add('scroll-header');
+    else nav.classList.remove('scroll-header')
+}
+
+window.addEventListener('scroll',scrollHeader)
+/*================= SHOW SCROLL TOP =================*/
+/*================= DARK LIGHT THEME =================*/
