575075ca565fee6fdf39ecaf2171a1d9ab3758ce
AmirHossein <amirhidevs@gmail.com>
2024-04-15 00:27:34 +0330

    change some styles

--- portfolio-2024/pages/skills.vue
+++ portfolio-2024/pages/skills.vue
@@ -7,12 +7,11 @@ definePageMeta({
 </script>
 
 <template>
-  <v-col cols="12" class="px-md-16">
-
-    <v-card height="auto" class="mx-md-16">
+  <v-col cols="12">
+    <v-card height="auto" max-width="1000">
       <!-- Introduction Section -->
       <div>
-        <v-card-text class="font-20 text-line-height font-weight-bold">
+        <v-card-text class="font-20 font-weight-bold">
           A
           <strong class="text-primary">full-stack software engineer</strong>
           with over
@@ -20,7 +19,7 @@ definePageMeta({
         </v-card-text>
 
         <v-card-text
-          class="text-justify font-16 text-line-height font-weight-thin"
+          class="text-justify font-16 font-weight-thin"
         >
           I have maintained, developed and launched multiple projects from
           scratch, handling the development of their back-end and front-end
@@ -41,7 +40,7 @@ definePageMeta({
 
         <v-card-text>
           <ul
-            class="ml-7 text-justify font-16 text-line-height font-weight-thin"
+            class="ml-7 text-justify font-16 font-weight-thin"
           >
             <li>leading or co-developing their back-ends and front-ends,</li>
             <li>automating the deployment process,</li>
@@ -56,71 +55,82 @@ definePageMeta({
         <v-card-text class="font-20">
           For examples of my work, check out my
           <nuxt-link
-            :to="'/homepage'"
+            to="/homepage"
             class="text-decoration-none text-primary"
           >
             portfolio.
           </nuxt-link>
         </v-card-text>
       </div>
-        <v-divider class="border-opacity-50 my-8" inset role="presentation" />
+      <v-divider class="border-opacity-50 my-8" inset role="presentation" />
       <!-- Front-end Section -->
       <div>
-          <v-card-text class="font-20 text-line-height font-weight-bold">
-            A
-            <strong class="text-primary">full-stack software engineer</strong>
-            with over
-            <strong class="text-primary">15 years of experience</strong>.
-          </v-card-text>
+        <v-card-text class="font-20 font-weight-bold">
+          A
+          <strong class="text-primary">back-end software engineer</strong>
+          with over
+          <strong class="text-primary">15 years of experience</strong>.
+        </v-card-text>
 
-          <v-card-text
-            class="text-justify font-16 text-line-height font-weight-thin"
+        <v-card-text
+          class="text-justify font-16 font-weight-thin"
+        >
+          I have maintained, developed and launched multiple projects from
+          scratch, handling the development of their back-end and front-end
+          codebases.
+          <br />
+          My current toolset is composed of JavaScript & TypeScript (including
+          Node.js), React, Kubernetes, Docker, and AWS CDK, Pulumi or Terraform.
+          <br />
+          Lately, I am also finding myself using low-code / no-code tools more
+          and more, such as Plasmic and Xano. These tools can deliver projects
+          of great quality while allowing for much greater flexibility, at a
+          fraction of the usual cost.
+        </v-card-text>
+
+        <v-card-text class="font-weight-bold font-18"
+          >I can assist you with all aspects of your project:
+        </v-card-text>
+
+        <v-card-text>
+          <ul
+            class="ml-7 text-justify font-16 font-weight-thin"
           >
-            I have maintained, developed and launched multiple projects from
-            scratch, handling the development of their back-end and front-end
-            codebases.
-            <br />
-            My current toolset is composed of JavaScript & TypeScript (including
-            Node.js), React, Kubernetes, Docker, and AWS CDK, Pulumi or
-            Terraform.
-            <br />
-            Lately, I am also finding myself using low-code / no-code tools more
-            and more, such as Plasmic and Xano. These tools can deliver projects
-            of great quality while allowing for much greater flexibility, at a
-            fraction of the usual cost.
-          </v-card-text>
+            <li>leading or co-developing their back-ends and front-ends,</li>
+            <li>automating the deployment process,</li>
+            <li>improving UI/UX design,</li>
+            <li>mentoring the team,</li>
+            <li>researching possible techs,</li>
+            <li>setting up monitoring,</li>
+            <li>improving the development process in general.</li>
+          </ul>
+        </v-card-text>
 
-          <v-card-text class="font-weight-bold font-18"
-            >I can assist you with all aspects of your project:
-          </v-card-text>
+        <v-card-text class="font-20">
+          For examples of my work, check out my
+          <nuxt-link
+            to="/homepage"
+            class="text-decoration-none text-primary">portfolio</nuxt-link>.
+        </v-card-text>
+      </div>
+      <v-divider class="border-opacity-50 my-8" role="presentation" />
+<!--       Final Section -->
+      <div>
+        <v-card-text class="text-justify font-16 font-weight-thin">
+          If your initial thought is that I can’t be the best in everything,
+          it's okay. Send the design job to someone else, I will take care of
+          all the code development or any other role you see me best suited for.
+        </v-card-text>
 
-          <v-card-text>
-            <ul
-              class="ml-7 text-justify font-16 text-line-height font-weight-thin"
-            >
-              <li>leading or co-developing their back-ends and front-ends,</li>
-              <li>automating the deployment process,</li>
-              <li>improving UI/UX design,</li>
-              <li>mentoring the team,</li>
-              <li>researching possible techs,</li>
-              <li>setting up monitoring,</li>
-              <li>improving the development process in general.</li>
-            </ul>
-          </v-card-text>
-
-          <v-card-text class="font-20">
-            For examples of my work, check out my
-            <nuxt-link
-              :to="'/homepage'"
-              class="text-decoration-none text-primary"
-            >
-              portfolio.
-            </nuxt-link>
-          </v-card-text>
-        </div>
+        <v-card-text class="text-justify font-16 font-weight-thin">
+          In cases where you already have an existing team but still need some
+          help, feel free to
+          <nuxt-link to="/homepage" class="text-decoration-none text-primary"
+            >contact me</nuxt-link>.
+          I assure you that I will fit in quickly.
+        </v-card-text>
+      </div>
     </v-card>
 
   </v-col>
 </template>
-
-
