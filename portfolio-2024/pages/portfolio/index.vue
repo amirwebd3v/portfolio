1eb4386a4ac0142e026b00d302ff51e834c853ba
AmirHossein <amirhidevs@gmail.com>
2024-04-28 16:59:35 +0330

    fix typescript issues of portfolio API route

--- portfolio-2024/pages/portfolio/index.vue
+++ portfolio-2024/pages/portfolio/index.vue
@@ -1,4 +1,6 @@
 <script setup lang="ts">
+import type { PortfolioType } from '~/server/api/portfolio'
+
 definePageMeta({
   icon: 'mdi-view-gallery',
   title: 'Portfolio',
@@ -7,13 +9,13 @@ definePageMeta({
 
 const { data } = await useFetch('/api/portfolio')
 
-const projects = ref(data)
+const projects: PortfolioType[] = [...data.value]
 
-const goToProject = (projectTitle: string) => {
-  if (projectTitle) {
+const goToProject = (title: PortfolioType['title']) => {
+  if (title) {
     useRouter().push({
-      name: 'portfolio-title',
-      params: { title : projectTitle},
+      name: 'portfolio-project',
+      params: { project : title },
     })
   }
 }
@@ -47,51 +49,60 @@ usePageShow('portfolio')
             By the way, I possess skills in a variety of tools and technologies,
             which are listed below.
           </p>
-          <v-row justify="space-evenly" class="py-8">
-            <v-icon class="pa-1" size="x-large" icon="custom:TypeScript" />
-            <v-icon class="pa-1" size="x-large" icon="custom:JavaScript" />
-            <v-icon class="pa-1" size="x-large" icon="custom:Nuxt JS" />
-            <v-icon class="pa-1" size="x-large" icon="custom:Vue.js" />
-            <v-icon class="pa-1" size="x-large" icon="custom:Veutify" />
-            <v-icon class="pa-1" size="x-large" icon="custom:Bootstrap" />
-            <v-icon class="pa-1" size="x-large" icon="custom:Laravel" />
+          <v-row justify="space-evenly" align="center" class="my-10">
+            <v-icon class="pa-1" size="xxx-large" icon="custom:TypeScript" />
+            <v-icon class="pa-1" size="xxx-large" icon="custom:JavaScript" />
+            <v-icon class="pa-1" size="xxx-large" icon="custom:Nuxt JS" />
+            <v-icon class="pa-1" size="xxx-large" icon="custom:Vue.js" />
+            <v-icon class="pa-1" size="xxx-large" icon="custom:Veutify" />
+            <v-icon class="pa-1" size="xxx-large" icon="custom:Bootstrap" />
+            <v-icon class="pa-1" size="xxx-large" icon="custom:Laravel" />
             <v-icon class="pa-1" size="xxx-large" icon="custom:Docker" />
-            <v-icon class="pa-1" size="x-large" icon="custom:PostgresSQL" />
-            <v-icon class="pa-1" size="x-large" icon="custom:MySQL" />
-            <v-icon class="pa-1" size="x-large" icon="custom:Adobe Photoshop" />
+            <v-icon class="pa-1" size="xxx-large" icon="custom:PostgresSQL" />
+            <v-icon class="pa-1" size="xxx-large" icon="custom:MySQL" />
+            <v-icon class="pa-1" size="xxx-large" icon="custom:Adobe Photoshop" />
             <v-icon
               class="pa-1"
               size="x-large"
               icon="custom:Adobe Illustrator"
             />
           </v-row>
-        </v-card-text>
 
-        <div v-for="project in projects" :key="project">
-          <v-divider />
+          <p>
+            For more details about my experiences, please take a look at
+            <nuxt-link to="/cv" class="text-decoration-none text-primary"
+          >my CV</nuxt-link>.
+          </p>
+        </v-card-text>
+      </div>
+
+
+
+        <div v-for="project in projects" :key="project.title">
+          <v-divider class="mt-8" />
           <v-row class="pt-5">
             <v-col
               class="flex-grow-1 flex-shrink-1 order-1"
               cols="auto"
               md="6"
               sm="12"
-              style="min-width: auto; max-width: 100%"
+              style="min-width: auto; max-width: 100%;"
             >
               <v-card-title>
                 <h2 class="font-weight-bold">{{ project.title }}</h2>
               </v-card-title>
 
-              <v-card-text class="font-weight-thin font-18">
+              <v-card-text class="font-weight-thin font-20">
                 {{ project.subtitle }}
               </v-card-text>
 
-              <v-card-text class="font-weight-bold font-18 text-justify">
+              <v-card-text class="font-weight-bold font-18 text-justify pt-0">
                 {{ project.shortDescription }}
               </v-card-text>
 
-              <v-card-text>
+              <v-card-text class="pt-0">
                 <ul
-                  class="ml-7 text-justify font-18 font-weight-thin"
+                  class="ml-10 text-justify font-18 font-weight-thin"
                   style="list-style-type: circle"
                 >
                   <li v-for="task in project.tasks">
@@ -105,7 +116,8 @@ usePageShow('portfolio')
                   rounded="md"
                   elevation="2"
                   @click="goToProject(project.title)"
-                  >Read more...
+                >
+                  Read more...
                 </v-btn>
               </v-card-text>
             </v-col>
@@ -118,12 +130,12 @@ usePageShow('portfolio')
               style="min-width: 210px; max-width: 100%"
             >
               <v-card-text>
-                <v-img rounded="lg" :src="project.images[0]"></v-img>
+                <v-img rounded="lg" :src="project.images[0]" />
               </v-card-text>
             </v-col>
           </v-row>
         </div>
-      </div>
+
     </v-card>
   </v-col>
 </template>
--- portfolio-2024/server/api/portfolio.ts
+++ portfolio-2024/server/api/portfolio.ts
@@ -1,8 +1,30 @@
-import jsonData from '@/server/api/data/portfolio.json'
 
-export default defineEventHandler(async (event) => {
+import projects from "@/server/api/data/portfolio.json";
+
+
+interface PortfolioType {
+  title: string;
+  subtitle: string;
+  shortDescription: string;
+  tasks: string[];
+  fullDescription: string;
+  techWrapUp: TechWrapUp[];
+  images: string[];
+}
+
+interface TechWrapUp {
+  first: string;
+  second: string;
+}
+
+
+export {PortfolioType}
+
+
+
+export default defineEventHandler( event => {
   // Handle the API route
   if (event.node.req.url === '/api/portfolio') {
-    return jsonData
+    return projects as PortfolioType[]
   }
 })
